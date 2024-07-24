229e21711a57eed98a72302e7fe365561b1c5f86
ckdash-git <mail.ckdash@gmail.com>
2024-07-24 11:29:38 +0530

    Initial Commit

---
+++ Baat/BaatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaatApp.swift
+//  Baat
+//
+//  Created by Chandan Kumar Dash on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BaatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Baat
+//
+//  Created by Chandan Kumar Dash on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
